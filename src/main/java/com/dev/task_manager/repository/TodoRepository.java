436773ea392d9b2f7c43132ada77f565633a7624
nilsson46 <nilssonsimon46@gmail.com>
2025-01-21 21:58:31 +0100

    Repository todo added

---
+++ src/main/java/com/dev/task_manager/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.dev.task_manager.repository;
+
+import com.dev.task_manager.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
