a98633fc0ebc29876657cdfe9dd27b7eddc8aba1
nilsson46 <nilssonsimon46@gmail.com>
2025-01-21 22:07:04 +0100

    Controller todo added

---
+++ src/main/java/com/dev/task_manager/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.dev.task_manager.controller;
+
+import com.dev.task_manager.model.Todo;
+import com.dev.task_manager.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
+        Todo newTodo = todoService.addTodo(todo);
+        return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
+    }
+}
